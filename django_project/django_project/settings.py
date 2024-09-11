97febfdccbb0e6bb0971bad7e8bfff5a5c358d86
Orlovchik <svetl.orlovaa@gmail.com>
2024-09-11 12:01:31 +0300

    ch13: Image Book Cover Upload

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -125,9 +125,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
@@ -137,8 +134,11 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = "/media/"
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
